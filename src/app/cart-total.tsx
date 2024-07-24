b593e8cb57ee9fe369bf42a0f57a09e606ff0e40
Pushan T <pushan.sahanaganesh@gmail.com>
2024-07-25 01:18:55 +0530

    SSR issue resolved

--- src/app/cart-total.tsx
+++ src/app/cart-total.tsx
@@ -10,45 +10,9 @@ import {
 } from "@/components/ui/sheet";
 
 import { useCart } from "@/context/cart-context";
-import { foodItems } from "./page";
+import { foodItems } from "./temp_data";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-
-type FoodItem = {
-    name: string;
-    image: string;
-    description: string;
-    price: number;
-    isVeg: boolean;
-    category: string;
-};
-const foodItems: FoodItem[] = [
-    {
-        name: "Veg Sandwich",
-        image: "https://2.bp.blogspot.com/-VDlK83TTN04/VgEVZa6XZfI/AAAAAAAAH6k/V7jLCOWXaHU/s1600/spicy%20vegetable%20sandwich.JPG",
-        description:
-            "A delicious sandwich with fresh vegetables with a side of fries",
-        price: 150,
-        isVeg: true,
-        category: "Munchies",
-    },
-    {
-        name: "Cold Coffee",
-        image: "https://rachnas-kitchen.com/wp-content/uploads/2017/07/cold-coffee-2.jpg",
-        description: "A refreshing cold coffee with a hint of chocolate",
-        price: 100,
-        isVeg: true,
-        category: "Bevarages",
-    },
-    {
-        name: "Chicken Alfredo Pasta",
-        image: "https://www.foodwerk-blog.de/wp-content/uploads/2022/03/IMG_3657-1-980x1225.jpg",
-        description: "A creamy pasta with coocked chicken",
-        price: 200,
-        isVeg: false,
-        category: "Meal",
-    },
-];
 
 function CartTotal({ className }: { className?: string }) {
     const { cartItems } = useCart();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,42 +11,7 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import CartTotal from "./cart-total";
-
-export type FoodItem = {
-    name: string;
-    image: string;
-    description: string;
-    price: number;
-    isVeg: boolean;
-    category: string;
-};
-export const foodItems: FoodItem[] = [
-    {
-        name: "Veg Sandwich",
-        image: "https://2.bp.blogspot.com/-VDlK83TTN04/VgEVZa6XZfI/AAAAAAAAH6k/V7jLCOWXaHU/s1600/spicy%20vegetable%20sandwich.JPG",
-        description:
-            "A delicious sandwich with fresh vegetables with a side of fries",
-        price: 150,
-        isVeg: true,
-        category: "Munchies",
-    },
-    {
-        name: "Cold Coffee",
-        image: "https://rachnas-kitchen.com/wp-content/uploads/2017/07/cold-coffee-2.jpg",
-        description: "A refreshing cold coffee with a hint of chocolate",
-        price: 100,
-        isVeg: true,
-        category: "Bevarages",
-    },
-    {
-        name: "Chicken Alfredo Pasta",
-        image: "https://www.foodwerk-blog.de/wp-content/uploads/2022/03/IMG_3657-1-980x1225.jpg",
-        description: "A creamy pasta with coocked chicken",
-        price: 200,
-        isVeg: false,
-        category: "Meal",
-    },
-];
+import { foodItems } from "./temp_data";
 
 export default async function Home() {
     const supabase = createClient();
